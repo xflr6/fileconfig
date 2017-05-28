0f9249fe3666b333c36886d39b2b9862cffb68ee
Sebastian Bank <sebastian.bank@uni-leipzig.de>
2017-05-28 08:07:25 +0200

    release 0.5.5

--- fileconfig/__init__.py
+++ fileconfig/__init__.py
@@ -7,7 +7,7 @@ from .bases import Config, Stacked
 __all__ = ['Config', 'Stacked']
 
 __title__ = 'fileconfig'
-__version__ = '0.5.5.dev0'
+__version__ = '0.5.5'
 __author__ = 'Sebastian Bank <sebastian.bank@uni-leipzig.de>'
 __license__ = 'MIT, see LICENSE'
-__copyright__ = 'Copyright (c) 2014-2015 Sebastian Bank'
+__copyright__ = 'Copyright (c) 2014-2015,2017 Sebastian Bank'
