5225f2ce211a28540d6f47a4afe61b8e71374776
Sebastian Bank <sebastian.bank@uni-leipzig.de>
2018-05-24 01:37:07 +0200

    bump version for development

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='fileconfig',
-    version='0.5.7',
+    version='0.5.8.dev0',
     author='Sebastian Bank',
     author_email='sebastian.bank@uni-leipzig.de',
     description='Config file sections as objects',
