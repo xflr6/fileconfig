f8207331df21d945c40cebf835cc15d7fee9a288
Sebastian Bank <sebastian.bank@uni-leipzig.de>
2020-05-11 14:57:20 +0200

    upgrade pytest to >=4

--- setup.py
+++ setup.py
@@ -19,8 +19,8 @@ setup(
     platforms='any',
     python_requires='>=2.7,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*,!=3.4.*',
     extras_require={
-        'dev': ['flake8', 'pep8-naming', 'wheel', 'twine'],
-        'test': ['pytest>=3.4,!=3.10.0', 'pytest-cov'],
+        'dev': ['tox>3', 'flake8', 'pep8-naming', 'wheel', 'twine'],
+        'test': ['pytest>=4', 'pytest-cov'],
     },
     long_description=io.open('README.rst', encoding='utf-8').read(),
     classifiers=[
